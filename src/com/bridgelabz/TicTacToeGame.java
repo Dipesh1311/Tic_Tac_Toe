44573f53c822f931afd1713609e926e1e8c769a7
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 19:54:57 +0530

    First Program for tic tac toe

---
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class TicTacToeGame {
+
+   static char[] arr = new char[10];
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        TicTacToeGame t1 = new TicTacToeGame();
+        arr[0]='N';
+    }
+}
