e12efa2afaa5e6724b518e07380ed126103fa8bc
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-05-25 20:35:55 +0530

    Added Program to take player input

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,13 +1,38 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
-   static char[] arr = new char[10];
+    static char[] board = new char[10];
+    static char computer;
+    static char player;
+    public static Scanner scannerObject = new Scanner(System.in);
+
+    static void createBoard(){
+        for(int index = 1 ; index<10 ; index++) {
+            board[index] = ' ';
+        }
+    }
+
+    public static void getPlayerChoice() {
+
+        System.out.print("Choose X or O : ");
+        player = Character.toUpperCase(scannerObject.next().charAt(0));
+
+        if (player == 'X')
+            computer = 'O';
+        else
+            computer = 'X';
+        System.out.println("You have selected : " + player);
+        System.out.println("Computer's choice is : " + computer);
+    }
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         TicTacToeGame t1 = new TicTacToeGame();
-        arr[0]='N';
+        getPlayerChoice();
+        createBoard();
     }
 }
