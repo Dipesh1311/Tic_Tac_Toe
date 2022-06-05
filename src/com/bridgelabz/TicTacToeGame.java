323f174342c29e789eeb08a221feb8831937cec2
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-05 11:06:50 +0530

    Added implemented program for User movement in free space.

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
+    public static Scanner scannerObject = new Scanner(System.in);
     static char[] board = new char[10];
-
     static char player,computer;
-    public static Scanner scanner = new Scanner(System.in);
     private static int playLocation;
-    static void createBoard(){
+
+    public static void createBoard(){
         for(int index = 1 ; index<10 ; index++) {
             board[index] = ' ';
         }
     }
 
-    static void getPlayerChoice() {
+    public static void getPlayerChoice() {
+
         System.out.print("select X or O : ");
-        player = Character.toUpperCase(scanner.next().charAt(0));
+        player = Character.toUpperCase(scannerObject.next().charAt(0));
 
         if (player == 'X')
             computer='O';
@@ -27,29 +28,45 @@ public class TicTacToeGame {
         System.out.println("Computer's choice is : " +computer);
     }
 
-    static void showBoard() {
+
+    public static void showBoard() {
+        System.out.println();
         System.out.println("  " + board[1] + "  |  " + board[2]  + "   | " + board[3] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
+        System.out.println("------------------");
         System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
     }
+
+
     public static void userMove() {
         System.out.println("Enter Location 1-9 to Make Move");
-        playLocation = scanner.nextInt();
-        if (playLocation < 10 && playLocation > 0) {
+        playLocation = scannerObject.nextInt();
+        scannerObject.nextLine();
+        if (isEmpty() && playLocation < 10 && playLocation > 0) {
             board[playLocation] = player;
             showBoard();
         } else {
             System.out.println("Invalid Choice");
         }
-
     }
+
+    public static boolean isEmpty() {
+        if (board[playLocation] == ' ') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
+        System.out.println("----- Welcome To The Game Of Tic Tac Toe -----\n");
         createBoard();
         getPlayerChoice();
         showBoard();
         userMove();
+
 
     }
 
