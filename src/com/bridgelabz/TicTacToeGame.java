1256df1aff966eeff996d19f4785e9c87e21149a
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-05 10:55:22 +0530

    Added implemented program for User movement in the board.

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class TicTacToeGame {
 
     static char[] board = new char[10];
+
     static char player,computer;
     public static Scanner scanner = new Scanner(System.in);
-
+    private static int playLocation;
     static void createBoard(){
         for(int index = 1 ; index<10 ; index++) {
             board[index] = ' ';
@@ -31,13 +32,24 @@ public class TicTacToeGame {
         System.out.println("  " + board[4] + "  |  " + board[5]  + "   | " + board[6] + "  ");
         System.out.println("  " + board[7] + "  |  " + board[8]  + "   | " + board[9] + "  ");
     }
+    public static void userMove() {
+        System.out.println("Enter Location 1-9 to Make Move");
+        playLocation = scanner.nextInt();
+        if (playLocation < 10 && playLocation > 0) {
+            board[playLocation] = player;
+            showBoard();
+        } else {
+            System.out.println("Invalid Choice");
+        }
 
+    }
     public static void main(String[] args) {
 
         System.out.println("----- Welcome To The Game Of Tic Tac Toe -----");
         createBoard();
         getPlayerChoice();
         showBoard();
+        userMove();
 
     }
 
